8d39c7a975a4b862550e8d818f95d539a6b8bc83
Daniel Rowe <rowedanielp@gmail.com>
2021-03-04 13:36:59 -0800

    updated quiz security to prevent people from stealing answers

--- server_scripts/QuizManager.py
+++ server_scripts/QuizManager.py
@@ -1,16 +1,19 @@
+import hashlib
+
+
 
 class Quiz:
     __slots__ = ('questionFilename','answerFilename',
                  'players', 'questions', 'answers')
 
-    def __init__(self, questionFilename, answerFilename):
+    def __init__(self, questionFilename, answerFilename, firstAnswer):
 
         self.questionFilename = questionFilename
         self.answerFilename = answerFilename
         
         self.players = {}
         self.questions = ["Type 'start'"]
-        self.answers = ["start"]
+        self.answers = [firstAnswer]
         self.load_quiz()
 
     async def add_player(self, sid):
@@ -18,7 +21,8 @@ class Quiz:
             self.players[sid] = 0
 
     async def remove_player(self, sid):
-        del self.players[sid]
+        if sid in self.players:
+            del self.players[sid]
 
     async def get_question(self, sid):
         if sid not in self.players or \
@@ -49,6 +53,8 @@ class Quiz:
     async def add_question(self, sid, question, answer):
         if sid not in self.players or self.players[sid] != -1:
             return False
+
+        answer = answer
         
         # add new question/answer to the live list
         self.questions.append(question)
@@ -63,43 +69,62 @@ class Quiz:
 
 class QuizManager:
 
-    __slots__ = ( 'sio', 'mainQuiz', 'extraQuiz')
+    __slots__ = ( 'sio', 'firstAnswer', 'quizes', 'changeQuizKeywords')
 
     def __init__(self, sio):
         self.sio = sio
-        self.mainQuiz = Quiz('quiz/quizQuestions.txt',
-                             'quiz/quizAnswers.txt')
-        self.extraQuiz = Quiz('quiz/extraQuizQuestions.txt',
-                              'quiz/extraQuizAnswers.txt') 
+
+        self.firstAnswer = self.hash('start')
+        self.quizes = [Quiz('quiz/quizQuestions.txt',
+                            'quiz/quizAnswers.txt',
+                            self.firstAnswer),
+                       Quiz('quiz/extraQuizQuestions.txt',
+                            'quiz/extraQuizAnswers.txt',
+                            self.firstAnswer)
+                       ]
+        self.changeQuizKeywords = {
+            '889ed27c6a43a3efe392fe84cd85a9b211c30c0d':self.quizes[1]
+            }
+
+    def hash(self, msg):
+        return hashlib.sha1(msg.rstrip().encode()).hexdigest()
         
     async def handle_startQuiz(self, sid, data):
-        await self.mainQuiz.add_player(sid)
+        await self.quizes[0].add_player(sid)
         await self.sio.emit("startQuiz", {}, room=sid)
         
     async def handle_quizAttemptAnswer(self, sid, data):
+        if 'answer' not in data: return
         # checkpoint 1
-        if data['answer'] == 'OBOhcX8B3ruzfvOwFdgQ':
-            await self.mainQuiz.remove_player(sid)
-            await self.extraQuiz.add_player(sid)
-            await self.attempt_advance(self.extraQuiz, sid, 'start')
+        
+        answer = self.hash(data['answer'])
+        
+        if answer in self.changeQuizKeywords:
+            for quiz in self.quizes:
+                await quiz.remove_player(sid)
+            newQuiz = self.changeQuizKeywords[answer]
+            await newQuiz.add_player(sid)
+            await self.attempt_advance(newQuiz, sid, self.firstAnswer)
             
         # check if anwer works for either quiz
-        elif not await self.attempt_advance(self.mainQuiz,
-                                            sid,
-                                            data['answer']) and \
-             not await self.attempt_advance(self.extraQuiz,
-                                            sid,
-                                            data['answer']):
-            # if not, signal the player that it was wrong
-            await self.sio.emit('quizWrongAnswer', {}, room=sid)
+        else:
+            rightAnswer = False
+            for quiz in self.quizes:
+                if await self.attempt_advance(quiz,
+                                              sid,
+                                              answer):
+                    rightAnswer = True
+            if not rightAnswer:
+                await self.sio.emit('quizWrongAnswer', {}, room=sid)
     
     async def handle_quizSubmitQuestion(self, sid, data):
-        await self.mainQuiz.add_question(sid,
-                                         data['question'],
-                                         data['answer'])
-        await self.extraQuiz.add_question(sid,
-                                          data['question'],
-                                          data['answer'])
+        if 'question' not in data or 'answer' not in data: return
+        question = data['question']
+        answer = self.hash(data['answer'])
+        for quiz in self.quizes:
+            await quiz.add_question(sid,
+                                    question,
+                                    answer)
 
 
         
